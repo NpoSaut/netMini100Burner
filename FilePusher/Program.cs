bcb03bed1df0ab079d4638a9af807b570dc78c39
Evgeniy Plyusnin <eninbox@gmail.com>
2014-09-12 17:26:45 +0600

    Начат проект по проталкиванию файла через USB

---
+++ FilePusher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using LibUsbDotNet;
+using SautDnw;
+
+namespace FilePusher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2) PrintHelp();
+
+            string fileName = args[0];
+            UInt16 startAddress = UInt16.Parse(args[1], NumberStyles.HexNumber);
+            Console.WriteLine("Файл: {0}", fileName);
+            Console.WriteLine("Адрес: {0:X4}", startAddress);
+            Console.WriteLine("Устанавливаем соединение с mini100");
+            using (var connection = S5Pc100UsbConnection.Establish())
+            {
+                Console.WriteLine("Передаём файл");
+                var package = DnwPacker.EnpackFile(fileName, startAddress);
+                connection.Write(package.GetBuff());
+            }
+            Console.WriteLine("Закончили");
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("Программа для записи файла в mini100");
+            Console.WriteLine("Параметры запуска:");
+            Console.WriteLine("    {имя_файла} {стартовый_адрес}");
+            Console.WriteLine("    {имя_файла} - путь к файлу, который нужно прошить");
+            Console.WriteLine("    {стартовый_адрес} - адрес, с которого нужно начать прошивку (в hex)");
+        }
+    }
+}
---
+++ FilePusher/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("FilePusher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FilePusher")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b755c3dc-442b-488f-85b6-a1afda4cd6bb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
